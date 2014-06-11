cc4f48cb416e2d7074150fd3f9a459d896dfba1c
drusz <sergei.b.ru+g@gmail.com>
2014-06-11 00:31:04 -0700

    add cache tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,9 @@
 import unittest
 
+from mock import Mock
+from werkzeug.contrib.cache import SimpleCache
+
+import uber
 from uber import app
 
 
@@ -10,8 +14,19 @@ class UberTestCase(unittest.TestCase):
 
         self.app = app.test_client()
 
+        self.cache = SimpleCache()
+
+        self.orig_set = uber.cache.set
+        self.orig_get_many = uber.cache.get_many
+
+        uber.cache.set = Mock(wraps=self.cache.set)
+        uber.cache.get_many = Mock(wraps=self.cache.get_many)
+
     def tearDown(self):
-        pass
+        self.cache.clear()
+
+        uber.cache.set = self.orig_set
+        uber.cache.get_many = self.orig_get_many
 
 
 if __name__ == '__main__':
