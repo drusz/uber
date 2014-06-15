3fbb5e234dd9881949c0c85972dfebb054fe6760
drusz <sergei.b.ru+g@gmail.com>
2014-06-15 00:09:39 -0700

    files for uwsgi deployment

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from werkzeug.wsgi import DispatcherMiddleware
 from uber import api, frontend
 
 
-if __name__ == '__main__':
-    application = DispatcherMiddleware(frontend.create_app(), {
-        '/api': api.create_app()
-    })
+app = DispatcherMiddleware(frontend.create_app(), {
+    '/api': api.create_app()
+})
 
-    run_simple('0.0.0.0', 5000, application, use_reloader=True)
+if __name__ == '__main__':
+    run_simple('0.0.0.0', 5000, app, use_reloader=True)
